fedcdb5ceb935ef7b9462620cdb258311f82fce6
WAGNER <aewinformatica@gmail.com>
2019-09-24 20:13:33 -0300

    Inserindo um registro com persist.(que não nasceu gerenciado)

--- comece-assim-com-jpa/src/main/java/com/aewinformatica/comece_assim_com_jpa/App.java
+++ comece-assim-com-jpa/src/main/java/com/aewinformatica/comece_assim_com_jpa/App.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import com.aewinformatica.comece_assim_com_jpa.model.Cliente;
+
 /**
  * Hello world!
  *
@@ -13,11 +15,22 @@ public class App
     public static void main( String[] args )
     {
     	
-    	EntityManagerFactory entityManagerFactory = Persistence
-    			.createEntityManagerFactory("Cliente-PU");
-    	EntityManager entityManager = entityManagerFactory.createEntityManager(); 
-       System.out.println( "Hello World!" );
-    	 entityManager.close();
-         entityManagerFactory.close();
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Cliente-PU");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		System.out.println("Hello World!");
+		
+		Cliente cliente = new Cliente();
+				cliente.setId(1);
+				cliente.setNome("Padaria nossa Alegria");
+				
+				entityManager.getTransaction().begin();
+				
+//		        Inserindo um registro com persist.(que não nasceu gerenciado)
+				entityManager.persist(cliente);
+				
+				entityManager.getTransaction().commit();
+				
+		entityManager.close();
+		entityManagerFactory.close();
     }
 }
