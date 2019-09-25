d8815e322ab80c4b76a1ac5121b71534b14b58a6
WAGNER <aewinformatica@gmail.com>
2019-09-25 02:07:05 -0300

    Primeira consulta Listando Usuario

--- destrinchando-jpql-hibernate/src/main/java/com/aewinformatica/sistemausuarios/ConsultasComJPQL.java
+++ destrinchando-jpql-hibernate/src/main/java/com/aewinformatica/sistemausuarios/ConsultasComJPQL.java
@@ -1,8 +1,13 @@
 package com.aewinformatica.sistemausuarios;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import com.aewinformatica.sistemausuarios.model.Usuario;
 
 
 public class ConsultasComJPQL {
@@ -14,8 +19,20 @@ public class ConsultasComJPQL {
 		
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
+		primeiraConsulta(entityManager);
 					  entityManager.close();
 					  entityManagerFactory.close();
+		
+	}
+	
+	public static void primeiraConsulta(EntityManager entityManager) {
+		
+		String jpql = "select u from Usuario u";
+		
+		TypedQuery<Usuario>typedQuery = entityManager.createQuery(jpql,Usuario.class);
+				List<Usuario> lista =  typedQuery.getResultList();
+				
+				lista.forEach(u-> System.out.println(u.getId() + ", " + u.getNome()));
 		
 	}
 }
