dd847b0a8f333ecf79cf2f191324ad5d7caffa72
WAGNER <aewinformatica@gmail.com>
2019-09-25 01:35:14 -0300

    Configurando 2 Projeto- Persistence.xml

--- destrinchando-jpql-hibernate/src/main/java/com/aewinformatica/sistemausuarios/ConsultasComJPQL.java
+++ destrinchando-jpql-hibernate/src/main/java/com/aewinformatica/sistemausuarios/ConsultasComJPQL.java
@@ -1,8 +1,21 @@
 package com.aewinformatica.sistemausuarios;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
 public class ConsultasComJPQL {
 
 	public static void main(String[] args) {
+		
+		EntityManagerFactory entityManagerFactory = Persistence
+					.createEntityManagerFactory("Usuarios-PU");
+		
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+					  entityManager.close();
+					  entityManagerFactory.close();
 		
 	}
 }
