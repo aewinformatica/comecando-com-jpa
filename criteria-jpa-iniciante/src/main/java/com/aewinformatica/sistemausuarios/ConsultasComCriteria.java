66daf98a7c6cd2491a298a6befbfbaf3484dc2af
WAGNER <aewinformatica@gmail.com>
2019-09-26 04:40:56 -0300

    criteria-jpa-iniciante - primeiraConsulta

---
+++ criteria-jpa-iniciante/src/main/java/com/aewinformatica/sistemausuarios/ConsultasComCriteria.java
@@ -0,0 +1,279 @@
+package com.aewinformatica.sistemausuarios;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import com.aewinformatica.sistemausuarios.dto.UsuarioDTO;
+import com.aewinformatica.sistemausuarios.model.Configuracao;
+import com.aewinformatica.sistemausuarios.model.Dominio;
+import com.aewinformatica.sistemausuarios.model.Usuario;
+
+
+public class ConsultasComCriteria {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory entityManagerFactory = Persistence
+					.createEntityManagerFactory("Usuarios-PU");
+		
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		primeiraConsulta(entityManager);
+//		EscolhendoORetorno(entityManager);
+//		FazendoProjecoes(entityManager);
+//		PassandoParametros(entityManager);
+//		FazendoJoins(entityManager);
+//		FazendoLeftJoin(entityManager);
+//		carregamentoComJoinFetch(entityManager);
+//		filtrandoRegistros(entityManager);
+//		utilizandoOperadoresLogicos(entityManager);
+//		utilizandoOperadorIn(entityManager);
+//		ordenandoResultados(entityManager);
+//		paginandoResultados(entityManager);
+		
+		
+					  entityManager.close();
+					  entityManagerFactory.close();
+		
+	}
+	
+
+
+
+
+
+
+    public static void primeiraConsulta(EntityManager entityManager) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Usuario> criteriaQuery = criteriaBuilder.createQuery(Usuario.class);
+        Root<Usuario> root = criteriaQuery.from(Usuario.class);
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Usuario> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Usuario> lista = typedQuery.getResultList();
+        lista.forEach(u -> System.out.println(u.getId() + ", " + u.getNome()));
+
+        /*
+        String jpql = "select u from Usuario u";
+        TypedQuery<Usuario> typedQuery = entityManager.createQuery(jpql, Usuario.class);
+        List<Usuario> lista = typedQuery.getResultList();
+        lista.forEach(u -> System.out.println(u.getId() + ", " + u.getNome()));
+        */
+    }
+	
+	public static void EscolhendoORetorno(EntityManager entityManager) {
+		
+		String jpql = "select u.dominio from Usuario u where u.id = 1";
+		
+		TypedQuery<Dominio>typedQuery = entityManager.createQuery(jpql,Dominio.class);
+		Dominio dominio =  typedQuery.getSingleResult();
+		
+				  System.out.println(dominio.getId() + ", " + dominio.getNome());
+				  
+	    String jpqlNom = "select u.nome from Usuario u";
+	    
+	    //definindo o tipo de dado retornado String de Nomes e Listando usando ForEach e Lambda
+	    TypedQuery<String> typedQueryNom = entityManager.createQuery(jpqlNom,String.class);
+	    
+	    List<String> ListaNom = typedQueryNom.getResultList();
+	    
+	   ListaNom.forEach(nome->System.out.println(nome));
+	}
+	
+	public static void FazendoProjecoes(EntityManager entityManager) {
+		
+		String jpqlArr = "select id, login, nome from Usuario";
+		
+		TypedQuery<Object[]>typedQueryArr = entityManager.createQuery(jpqlArr,Object[].class);
+		List<Object[]>listaArr = typedQueryArr.getResultList();
+		
+		listaArr.forEach(arr->System.out.println(String.format("%s, %s, %s", arr)));
+		
+		String jpqlDto = "select new com.algaworks.sistemausuarios.dto.UsuarioDTO(id, login, nome)" +
+                "from Usuario";
+        TypedQuery<UsuarioDTO> typedQueryDto = entityManager.createQuery(jpqlDto, UsuarioDTO.class);
+        List<UsuarioDTO> listaDto = typedQueryDto.getResultList();
+        listaDto.forEach(u -> System.out.println("DTO: " + u.getId() + ", " + u.getNome()));
+		
+	}
+	
+	public static void PassandoParametros(EntityManager entityManager) {
+		
+		//parametro IdUsuario
+		String jpql = "select u from Usuario u where u.id = :idUsuario";
+		
+		//encavalando os metodos
+		TypedQuery<Usuario>typedQuery = entityManager.
+				createQuery(jpql,Usuario.class)
+				//nome do paramentro dentro da JPQL	
+				.setParameter("idUsuario", 1);
+					Usuario usuario = typedQuery.getSingleResult();
+					
+	   System.out.println(usuario.getId() + ", " + usuario.getNome());
+	   
+	   
+		//parametro loginUsuario
+		String jpqlLog = "select u from Usuario u where u.login = :loginUsuario";
+		
+		//encavalando os metodos
+		TypedQuery<Usuario>typedQueryLog = entityManager.
+				createQuery(jpqlLog,Usuario.class)
+				//nome do paramentro dentro da JPQL	
+				.setParameter("loginUsuario", "ria");
+					Usuario usuarioLog = typedQueryLog.getSingleResult();
+					
+	   System.out.println(usuarioLog.getId() + ", " + usuarioLog.getNome());
+		
+	}
+	
+	public static void FazendoJoins(EntityManager entityManager) {
+		//o uso do inner é facultativo
+		String jpql="select u from Usuario u inner join u.dominio d where d.id = 1";
+		
+		TypedQuery<Usuario> typedQuery = entityManager.createQuery(jpql, Usuario.class);
+			List<Usuario>listaUsuarios = typedQuery.getResultList();
+			
+			listaUsuarios.forEach(u->System.out.println(u.getId() + ", " + u.getNome()));
+		
+	}
+	
+	public static void FazendoLeftJoin(EntityManager entityManager) {
+		
+		//o uso do outer é facultativo
+		String jpql = "select u, c from Usuario u left outer join u.configuracao c";
+		
+		TypedQuery<Object[]>typedQuery = entityManager.createQuery(jpql, Object[]	.class);
+		
+		List<Object[]>lista = typedQuery.getResultList();
+		lista.forEach(arr->{
+						//fazendo Cast nos objetos
+			String out = ((Usuario) arr[0]).getNome();
+						 if(arr[1] == null) {
+							 out += ", NULL";
+						 }
+						 else {
+							 //fazendo Cast nos objetos
+							 out += ", " + ((Configuracao) arr[1]).getId();
+						 }
+						 System.out.println(out);
+		});
+		
+		
+	}
+	
+	public static void carregamentoComJoinFetch(EntityManager entityManager) {
+		
+		String jpql = "select u from Usuario u join fetch u.configuracao join fetch u.dominio";
+		
+		TypedQuery<Usuario>typedQuery = entityManager.createQuery(jpql, Usuario.class);
+		List<Usuario> lista = typedQuery.getResultList();
+					  lista.forEach(u->System.out.println(u.getId() + ", " + u.getNome()));
+					
+		
+	}
+	
+	public static void filtrandoRegistros(EntityManager entityManager) {
+		
+		//TIPOS DE FILTROS : LIKE , IS NULL , IS EMPTY, BETWEEN, >, <, >=, <=, =, <>
+		// IS NULL = select u from Usuario u where u.senha is null
+		// IS EMPTY = select d from Dominio d where d.usuarios is empty
+		
+//String jpql = "select u from Usuario u where u.nome like :nomeUsuario";// LIKE sem Concat do JPQL
+		String jpql = "select u from Usuario u where u.nome like concat (:nomeUsuario, '%')";
+		
+		TypedQuery<Usuario>typedQuery = entityManager.createQuery(jpql, Usuario.class).
+				setParameter("nomeUsuario", "cal");
+//				setParameter("nomeUsuario", "cal%");//sem Concat do JPQL
+		
+		List<Usuario> lista = typedQuery.getResultList();
+		System.out.println("Select com LIKE e concat:");
+					  lista.forEach(u->System.out.println(u.getId() + ", " + u.getNome()));
+	
+		//FILTRO BETWEEN			  
+	   String jpqlBet = "select u from Usuario u where u.ultimoAcesso between :ontem and :hoje";
+	   TypedQuery<Usuario>typedQueryBet = entityManager.createQuery(jpqlBet,Usuario.class)
+               .setParameter("ontem", LocalDateTime.now().minusDays(1))
+               .setParameter("hoje", LocalDateTime.now());
+	   List<Usuario> listaBet = typedQueryBet.getResultList();
+	   System.out.println("Select com Between:");
+       listaBet.forEach(ub -> System.out.println(ub.getId() + ", " + ub.getNome()));
+	   					
+		
+	}
+	
+	public static void utilizandoOperadoresLogicos(EntityManager entityManager) {
+		
+		//UTILIZANDO >
+		String jpql = "select u from Usuario u where u.ultimoAcesso > :ontem and ultimoAcesso < :hoje";
+		
+		TypedQuery<Usuario>typedQuery = entityManager.createQuery(jpql, Usuario.class)
+	                       .setParameter("ontem", LocalDateTime.now().minusDays(1))
+	                       .setParameter("hoje", LocalDateTime.now());
+		List<Usuario> lista = typedQuery.getResultList();
+		System.out.println("Select com > :");
+					  lista.forEach(u->System.out.println(u.getId() + ", " + u.getNome()));
+					  
+					  
+		//UTILIZANDO OR e IS NULL
+		String jpqlOR = "select u from Usuario u where"
+				+ " (u.ultimoAcesso > :ontem and ultimoAcesso < :hoje)"
+				+ "or ultimoAcesso is null";
+		
+		TypedQuery<Usuario>typedQueryOR = entityManager.createQuery(jpqlOR, Usuario.class)
+	                       .setParameter("ontem", LocalDateTime.now().minusDays(1))
+	                       .setParameter("hoje", LocalDateTime.now());
+		List<Usuario> listaOR = typedQueryOR.getResultList();
+		 System.out.println("Select com OR e IS NULL:");
+					  listaOR.forEach(uo->System.out.println(uo.getId() + ", " + uo.getNome()));
+		
+	}
+	public static void utilizandoOperadorIn(EntityManager entityManager) {
+		
+		String jpql = "select u from Usuario u where u.id in (:ids)";
+		
+		TypedQuery<Usuario>typedQuery = entityManager.createQuery(jpql, Usuario.class)
+							//passando uma lista de arrays para filtrar o usuario de codigo 1 e 2
+						   .setParameter("ids", Arrays.asList(1,2));
+		List<Usuario> lista = typedQuery.getResultList();
+					  lista.forEach(u->System.out.println(u.getId() + ", " + u.getNome()));
+		
+	}
+	
+	public static void ordenandoResultados(EntityManager entityManager) {
+		
+		String jpql = "select u from Usuario u order by u.nome";
+		
+		TypedQuery<Usuario>typedQuery = entityManager.createQuery(jpql, Usuario.class);
+		List<Usuario> lista = typedQuery.getResultList();
+					  lista.forEach(u->System.out.println(u.getId() + ", " + u.getNome()));
+					
+		
+	}
+	
+	public static void paginandoResultados(EntityManager entityManager) {
+		
+		String jpql = "select u from Usuario u";
+		
+		TypedQuery<Usuario>typedQuery = entityManager.createQuery(jpql, Usuario.class)
+						  //qual e o primeiro registro
+						  .setFirstResult(0)
+						  //mostrar de 2 e 2
+						  .setMaxResults(2);
+		List<Usuario> lista = typedQuery.getResultList();
+					  lista.forEach(u->System.out.println(u.getId() + ", " + u.getNome()));
+					
+		
+	}
+}
---
+++ criteria-jpa-iniciante/src/main/java/com/aewinformatica/sistemausuarios/model/Usuario.java
@@ -0,0 +1,157 @@
+package com.aewinformatica.sistemausuarios.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+	
+	@Id
+    private Integer id;
+
+    private String login;
+
+    private String senha;
+
+    private String nome;
+
+    private LocalDateTime ultimoAcesso;
+    
+    @ManyToOne
+    private Dominio dominio;
+    
+    @OneToOne(mappedBy = "usuario")
+    private Configuracao configuracao;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the login
+	 */
+	public String getLogin() {
+		return login;
+	}
+
+	/**
+	 * @param login the login to set
+	 */
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	/**
+	 * @return the senha
+	 */
+	public String getSenha() {
+		return senha;
+	}
+
+	/**
+	 * @param senha the senha to set
+	 */
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the ultimoAcesso
+	 */
+	public LocalDateTime getUltimoAcesso() {
+		return ultimoAcesso;
+	}
+
+	/**
+	 * @param ultimoAcesso the ultimoAcesso to set
+	 */
+	public void setUltimoAcesso(LocalDateTime ultimoAcesso) {
+		this.ultimoAcesso = ultimoAcesso;
+	}
+
+	/**
+	 * @return the dominio
+	 */
+	public Dominio getDominio() {
+		return dominio;
+	}
+
+	/**
+	 * @param dominio the dominio to set
+	 */
+	public void setDominio(Dominio dominio) {
+		this.dominio = dominio;
+	}
+	
+  public Configuracao getConfiguracao() {
+  return configuracao;
+}
+
+public void setConfiguracao(Configuracao configuracao) {
+  this.configuracao = configuracao;
+}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+    
+    
+
+}
